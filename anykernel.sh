30b81750467315d426901ea3c5455da84894cbdb
infectedmushi <infectedmushi@gmail.com>
2025-06-17 17:57:44 +0100

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=KernelSU-Next with SuSFS by deepongi
 do.devicecheck=0
 do.modules=0
 do.systemless=0
