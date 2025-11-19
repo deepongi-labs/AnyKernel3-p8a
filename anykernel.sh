24d697daaae88509b7fa455da4a7688e01598241
infectedmushi <infectedmushi@gmail.com>
2025-11-19 09:42:58 +0000

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KowSU with SuSFS by deepongi
+kernel.string=MKSU with SuSFS by deepongi
 do.devicecheck=0
 do.modules=0
 do.systemless=0
