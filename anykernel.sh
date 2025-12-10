2dff107d18c435a4cd3cdfabe4692d5bbeef398b
deepongi <infectedmushi@gmail.com>
2025-12-10 00:17:12 +0000

    update kernel string

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=KSU Kernel by deepongi @ deepongi-labs
+kernel.string=SukiSU Kernel by deepongi @ deepongi-labs
 do.devicecheck=1
 do.modules=0
 do.systemless=1
